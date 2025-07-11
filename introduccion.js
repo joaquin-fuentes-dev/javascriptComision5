3ce9b9ef490004f04ee20249db4953ff3ee8317f
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2025-07-10 21:23:57 -0300

    primer commit

---
+++ introduccion.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo desde visual studio code");
+
+let nombreCompleto = "Joaquin Fuentes";
+console.log(nombreCompleto);
+
+let edad = 23;
+console.log(edad);
+
+const esMayorDeEdad = true;
+console.log(esMayorDeEdad);
+
