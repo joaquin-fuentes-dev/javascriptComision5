eee21f6d2aa49a121a82dadbc23abd0241bf0613
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2025-07-15 20:16:24 -0300

    variables, tipos de datos y operadores

---
+++ teoria/2-variables-tiposDatos-operadores/app.js
@@ -0,0 +1,49 @@
+// Variables
+let edad = 40;
+
+const pi = 3.14;
+
+// esto esta deprecado, ya no se usa
+// var nombre = "joaquin";
+
+// Tipos de datos
+
+// cadenas de texto o STRING
+let fechaDeHoy = "15 de Julio";
+console.log(fechaDeHoy);
+// let fecha_de_hoy
+// let fecha-de-hoy - esto no funciona
+// numeros o NUMBER
+let precio = 200;
+console.log(precio);
+// booleanos
+let activo = true;
+console.log(activo);
+// null
+let nombre = null;
+console.log(nombre);
+// undefined
+let apellido;
+console.log(apellido);
+// objetos
+// arreglos
+console.log("------------------------------");
+// OPERADORES ARITMETICOS
+// let numA = 100;
+// let numB = 2;
+
+// let resultado = numA * 0.2;
+
+// console.log(resultado);
+
+//OPERADORES RELACIONALES
+let A = 50;
+let B = 5;
+let C = 1;
+let D = 0;
+
+// let resultado = false || false && true || true;
+
+A--;
+A--;
+console.log(A);
