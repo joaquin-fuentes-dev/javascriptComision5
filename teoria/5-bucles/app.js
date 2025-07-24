dfd92af94392a4a116547dc6788f26cd274ca0ba
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2025-07-24 20:35:55 -0300

    condicionales: teoria y ejercicios

---
+++ teoria/5-bucles/app.js
@@ -0,0 +1,31 @@
+// for (let i = 1; i <= cantidadDeVueltas; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i, "Es par");
+//   } else {
+//     console.log(i, "Es impar");
+//   }
+// }
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) continue;
+  console.log(i);
+}
+
+// let numeroDeLaSuerte = 10;
+// let numeroIngresaElusuario = parseInt(prompt("Ingrese el numero de la suerte"));
+
+// while (numeroIngresaElusuario != numeroDeLaSuerte) {
+//   console.log("Uh ese no era el numero");
+//   numeroIngresaElusuario = parseInt(
+//     prompt("Probemos de nuevo, ingrese otro numero")
+//   );
+// }
+
+// console.log("Felicidades! Adivinaste!!!");
+
+// let numero = 6;
+
+// do {
+//   numero++;
+//   console.log("Este mensaje se muestra al menos una vez");
+// } while (10 != 10);
