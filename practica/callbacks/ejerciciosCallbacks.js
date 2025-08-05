ec4a41168195c8589f1690e522659359c8c0ce28
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2025-08-05 20:51:49 -0300

    ejercicios metodos arrays

---
+++ practica/callbacks/ejerciciosCallbacks.js
@@ -0,0 +1,21 @@
+// 1- 
+// map - Convertir un array de grados Celsius a Fahrenheit
+// Enunciado:
+// Dado un array con temperaturas en grados Celsius, usá map para obtener un nuevo array con esas temperaturas en Fahrenheit.
+
+// Fórmula: F = C × 1.8 + 32
+
+
+let numeros = [30, 50, 10, 35]
+
+
+// ✅ 3. filter - Filtrar palabras con más de 5 letras
+// Enunciado:
+// Dado un array de strings, usá filter para quedarte solo con las palabras que tengan más de 5 caracteres.
+
+let palabras  = ["auto", "telescopio"]
+
+//  indexOf - Buscar la posición de una palabra específica
+// Enunciado:
+// Dado un array de palabras, usá indexOf para encontrar la 
+// posición de una palabra específica. Si no está, devolvé -1.
