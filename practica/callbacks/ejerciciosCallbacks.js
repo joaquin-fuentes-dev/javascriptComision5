913b555cea79de259c84fdd40e6d379b5fa7f122
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2025-08-05 20:52:48 -0300

    ordene el numero de los ejercicos

--- practica/callbacks/ejerciciosCallbacks.js
+++ practica/callbacks/ejerciciosCallbacks.js
@@ -1,4 +1,4 @@
-// 1- 
+// ✅ 1. 
 // map - Convertir un array de grados Celsius a Fahrenheit
 // Enunciado:
 // Dado un array con temperaturas en grados Celsius, usá map para obtener un nuevo array con esas temperaturas en Fahrenheit.
@@ -9,13 +9,13 @@
 let numeros = [30, 50, 10, 35]
 
 
-// ✅ 3. filter - Filtrar palabras con más de 5 letras
+// ✅ 2. filter - Filtrar palabras con más de 5 letras
 // Enunciado:
 // Dado un array de strings, usá filter para quedarte solo con las palabras que tengan más de 5 caracteres.
 
 let palabras  = ["auto", "telescopio"]
 
-//  indexOf - Buscar la posición de una palabra específica
+// ✅ 3. indexOf - Buscar la posición de una palabra específica
 // Enunciado:
 // Dado un array de palabras, usá indexOf para encontrar la 
 // posición de una palabra específica. Si no está, devolvé -1.
