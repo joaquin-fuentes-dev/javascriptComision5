62b87de52c3bb04794d96a33625135b8c2b45119
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2025-08-14 21:23:15 -0300

    localstorage y json

--- teoria/10-CRUD-Pelis/app.js
+++ teoria/10-CRUD-Pelis/app.js
@@ -1,7 +1,15 @@
-let peliculas = [];
+let peliculasJSON = localStorage.getItem("peliculas");
+let peliculas = JSON.parse(peliculasJSON) || [];
 
 // Funcion para Agregar pelicula
-function agregarPelicula(titulo, director, anio, calificacion) {
+function agregarPelicula() {
+  const titulo = prompt("Ingrese el titulo de la pelicula");
+  const director = prompt("Ingrese el nombre del director de la pelicula");
+  const anio = parseInt(prompt("Ingrese el año de estreno de la pelicula"));
+  const calificacion = parseFloat(
+    prompt("Ingrese la calificación de la pelicula")
+  );
+
   let nuevaPelicula = {
     id: Date.now(),
     titulo,
@@ -15,14 +23,13 @@ function agregarPelicula(titulo, director, anio, calificacion) {
   };
   peliculas.push(nuevaPelicula);
 
+  localStorage.setItem("peliculas", JSON.stringify(peliculas));
   console.log("✅ Película agregada:", nuevaPelicula); // Mostramos en consola
 }
 
-agregarPelicula("Ironman 2", "Franco Toledo", 2015, 9);
-agregarPelicula("Titanes de pacifico", "Lautaro", 2010, 8.5);
-
 // Funcion para Listar peliculas
 function listarPeliculas() {
+  peliculas = JSON.parse(localStorage.getItem("peliculas"));
   console.log("🎬 Lista de películas:");
   peliculas.forEach((pelicula) => {
     const { director, titulo, calificacion, anio } = pelicula;
@@ -31,30 +38,109 @@ function listarPeliculas() {
     );
   });
 }
-listarPeliculas();
 // Funcion para Buscar pelicula por nombre
-function buscarPeliPorNombre(nombreBuscado) {
+function buscarPeliPorNombre() {
+  let nombreBuscado = prompt(
+    "Ingrese el nombre de la pelicula que desea buscar"
+  );
   let peliculaEncontrada = peliculas.find(
     (pelicula) => pelicula.titulo === nombreBuscado
   );
   console.log("🔎 Resultado de búsqueda:");
   console.log(peliculaEncontrada?.titulo);
 }
-buscarPeliPorNombre("Titanes");
 // Funcion para Actualizar calificacion de una pelicula
-
+function actualizarCalificacion() {
+  let nombreDirector = prompt(
+    "Ingrese el nombre del director de la pelicula que quiere editar"
+  );
+  let nuevaCalificacion = parseFloat(
+    prompt("Ingrese la nueva calificacion de la pelicula")
+  );
+  peliculas = peliculas.map((pelicula) => {
+    if (pelicula.director.trim() === nombreDirector.trim()) {
+      // devolver todo lo que ya tenia la pelicula pero modificar solo su calificacion
+      return { ...pelicula, calificacion: nuevaCalificacion };
+    } else {
+      return pelicula;
+    }
+  });
+  localStorage.setItem("peliculas", JSON.stringify(peliculas));
+}
 
 // Funcion para Eliminar una pelicula
 
-function eliminarPelicula(nombreDirector) {
+function eliminarPelicula() {
+  let nombreDirector = prompt(
+    "Ingrese el nombre del director de la pelicula que quiere editar"
+  );
   // del listado buscamos una en particular
   let posicionDeLaPelicula = peliculas.findIndex(
     (pelicula) => pelicula.director === nombreDirector
   );
   // eliminar el elemento en la posicion ?????
   let peliculaEliminada = peliculas.splice(posicionDeLaPelicula, 1);
+  localStorage.setItem("peliculas", JSON.stringify(peliculas));
   console.log("🗑️ Película eliminada:", peliculaEliminada[0].titulo);
-  listarPeliculas();
 }
+// ESTO DEBEN HACER USTEDEES PARA LA CLASE SIGUIENTE!!!!!!
+function guardarEnLocalStorage() {}
 
-eliminarPelicula("Lautaro");
+function obtenerDelLocalStorage() {}
+// agregarPelicula();
+// listarPeliculas();
+// actualizarCalificacion("Lautaro", 10);
+// listarPeliculas();
+
+function mostrarMenu() {
+  let entrar = true;
+  while (entrar) {
+    let opcion = prompt(
+      "📽️ Bienvenido al CRUD de Películas\n\n" +
+        "1️⃣ Agregar película\n" +
+        "2️⃣ Listar películas\n" +
+        "3️⃣ Buscar por nombre\n" +
+        "4️⃣ Actualizar calificación\n" +
+        "5️⃣ Eliminar película\n" +
+        "6️⃣ Salir\n\n" +
+        "Ingresá una opción:"
+    );
+    switch (opcion) {
+      case "1":
+        console.log("Agregar pelicula");
+        agregarPelicula();
+        break;
+      case "2":
+        console.log("Listar peliculas");
+        listarPeliculas();
+        break;
+      case "3":
+        console.log("Buscar pelicula por nombre");
+        buscarPeliPorNombre();
+        break;
+      case "4":
+        console.log("Editar calificacion de pelicula");
+        actualizarCalificacion();
+        break;
+      case "5":
+        console.log("Eliminar pelicula");
+        eliminarPelicula();
+        break;
+      case "6":
+        entrar = false;
+        console.log("Salir");
+        break;
+      default:
+        console.log("Opcion invalida");
+        break;
+    }
+  }
+}
+mostrarMenu();
+
+// peliculas = ["Titanes del pacifico", "titanic", "shrek"];
+// const peliculasJSON = JSON.stringify(peliculas);
+// localStorage.setItem("peliculas", peliculasJSON);
+// const peliculasDelLocalStorageJSON = localStorage.getItem("peliculas");
+// const peliculasDelLocalStorage = JSON.parse(peliculasDelLocalStorageJSON);
+// console.log(peliculasDelLocalStorage);
